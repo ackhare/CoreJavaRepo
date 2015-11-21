9addd4f32189ec866db9b08e75fd245186ddf2ca
chetan <bairagisirfire@gmail.com>
2015-11-22 04:37:17 +0530

    exchnger phaer

---
+++ src/Experimental/buhu.java
@@ -0,0 +1,29 @@
+package Experimental;
+
+/**
+ * Created by chetan on 19/11/15.
+ */
+
+public class buhu {
+    public static void main(String[] args) {
+        System.out.println("cfmbhvdlkfjndbfjbldfbhlfdbhdljbndfbl");
+    }
+}
+/*
+/usr/lib/jvm/java-7-oracle/bin/java
+
+        -agentlib:hprof -Didea.launcher.port=7537
+
+        -Didea.launcher.bin.path=/home/chetan/Downloads/idea-IU-141.1532.4/bin -Dfile.encoding=UTF-8
+        -classpath
+
+        /usr/lib/jvm/java-7-oracle/jre/lib/jfr.jar:/usr/lib/jvm/java-7-oracle/jre/lib/plugin.jar:/usr/lib/jvm/java-7-oracle/jre/
+lib/resources.jar:/usr/lib/jvm/java-7-oracle/jre/lib/deploy.jar:/usr/lib/jvm/java-7-oracle/jre/lib/charsets.jar:/usr/lib
+/jvm/java-7-oracle/jre/lib/jfxrt.jar:/usr/lib/jvm/java-7-oracle/jre/lib/rt.jar:/usr/lib/jvm/java-7-oracle/jre/lib/manage
+ment-agent.jar:/usr/lib/jvm/java-7-oracle/jre/lib/jce.jar:/usr/lib/jvm/java-7-oracle/jre/lib/jsse.jar:/usr/lib/jvm/java-
+7-oracle/jre/lib/javaws.jar:/usr/lib/jvm/java-7-oracle/jre/lib/ext/localedata.jar:/usr/lib/jvm/java-7-oracle/jre/lib/ext
+/sunjce_provider.jar:/usr/lib/jvm/java-7-oracle/jre/lib/ext/sunpkcs11.jar:/usr/lib/jvm/java-7-oracle/jre/lib/ext/dnsns.j
+ar:/usr/lib/jvm/java-7-oracle/jre/lib/ext/zipfs.jar:/usr/lib/jvm/java-7-oracle/jre/lib/ext/sunec.jar:/home/chetan/IdeaPr
+ojects/CoreJavaRepo/out/production/CoreJavaRepo:/home/chetan/Downloads/junit-4.10.jar:/home/chetan/Downloads/idea-IU-141
+1532.4/lib/idea_rt.jar com.intellij.rt.execution.application.AppMain buhu
+*/
---
+++ src/Threading/WithoutPainThreadingJavaWorld/PhaserDemo/PhaserDemo.java
@@ -0,0 +1,86 @@
+package Threading.WithoutPainThreadingJavaWorld.PhaserDemo;
+
+/**
+ * Created by chetan on 17/11/15.
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.concurrent.Phaser;
+/*
+Listing 8 is based on the first code example in Phaser's Javadoc. This example shows how to use Phaser instead of
+CountDownLatch to control a one-shot action serving a variable number of threads.
+
+        The application creates a pair of runnable tasks that each report the time (in milliseconds relative to the Unix
+        epoch) at which its starts to run. Compile and run this application, and you should observe output that's
+        similar to the following:
+
+        Thread-0 running at 1366315297635 Thread-1 running at 1366315297635
+
+        As you would expect from countdown latch behavior, both threads start running at (in this case) the same time
+        even though a thread may have been delayed by as much as 349 milliseconds thanks to the presence of
+        Thread.sleep().
+
+        Comment out phaser.arriveAndAwaitAdvance(); // await all creation and you should now observe the threads
+        starting at radically different times, as illustrated below:
+
+        Thread-1 running at 1366315428871 Thread-0 running at 1366315429100
+*/
+public class PhaserDemo
+{
+    public static void main(String[] args)
+    {
+        List<Runnable> tasks = new ArrayList<>();
+        tasks.add(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                System.out.printf("%s running at %d%n",
+                        Thread.currentThread().getName(),
+                        System.currentTimeMillis());
+            }
+        });
+        tasks.add(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                System.out.printf("%s running at %d%n",
+                        Thread.currentThread().getName(),
+                        System.currentTimeMillis());
+            }
+        });
+        runTasks(tasks);
+    }
+//Most have same methods just mechanism is diffremt
+    static void runTasks(List<Runnable> tasks)
+    {
+        final Phaser phaser = new Phaser(1); // "1" to register self
+        // create and start threads
+        for (final Runnable task: tasks)
+        {
+            phaser.register();//registring thread with phraser
+            new Thread()
+            {
+                @Override
+                public void run()
+                {
+                    try
+                    {
+                        Thread.sleep(50+(int)(Math.random()*300));
+                    }
+                    catch (InterruptedException ie)
+                    {
+                        System.out.println("interrupted thread");
+                    }
+                    phaser.arriveAndAwaitAdvance(); // await all creation
+                    task.run();
+                }
+            }.start();
+        }
+
+        // allow threads to start and deregister self
+        phaser.arriveAndDeregister();
+    }
+}
