e6f2c11d4a772e6367f2df62d22af3ab62f4e0d1
chetan <bairagisirfire@gmail.com>
2016-12-13 13:47:27 +0530

    concurrent example demo

--- src/Threading/WithoutPainThreadingJavaWorld/Concurrent_Collections/CopyOnWriteArrayListDemo.java
+++ src/Threading/WithoutPainThreadingJavaWorld/Concurrent_Collections/CopyOnWriteArrayListDemo.java
@@ -3,6 +3,7 @@ package Threading.WithoutPainThreadingJavaWorld.Concurrent_Collections;
 /**
  * Created by chetan on 11/12/16.
  */
+
 import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
@@ -10,41 +11,45 @@ import java.util.List;
 
 import java.util.concurrent.CopyOnWriteArrayList;
 
-public class CopyOnWriteArrayListDemo
-{
-   public static void main(String[] args)
-   {
-      List<String> empList = new ArrayList<>();
-      empList.add("John Doe");
-      empList.add("Jane Doe");
-      empList.add("Rita Smith");
-      Iterator<String> empIter = empList.iterator();
-      while (empIter.hasNext())
-         try
-         {
+public class CopyOnWriteArrayListDemo {
+    public static void main(String[] args) {
+        List<String> empList = new ArrayList<>();
+        empList.add("John Doe");
+        empList.add("Jane Doe");
+        empList.add("Rita Smith");
+        Iterator<String> empIter = empList.iterator();
+        while (empIter.hasNext())
+            try {
+                System.out.println(empIter.next());
+                if (!empList.contains("Tom Smith"))
+                    empList.add("Tom Smith");
+                //so as we see tom smith was not added to result , it just got ignored
+            } catch (ConcurrentModificationException cme) {
+                System.err.println("attempt to modify list during iteration");
+                break;
+            }
+/*
+CopyOnWriteArrayList<E>: This class describes a thread-safe variant of ArrayList
+in which all mutative operations (e.g.,
+add and set) are implemented by making a fresh copy of the
+underlying array whenever an element is added or removed.
+However, in-progress iterations continue to work on the previous copy
+ (when the iterator was created). Although there's
+some cost to copying the array, this cost is acceptable in situations where there are many more iterations than
+modifications.
+ */
+        List<String> empList2 = new CopyOnWriteArrayList<>();
+        empList2.add("John Doe");
+        empList2.add("Jane Doe");
+        empList2.add("Rita Smith");
+        empIter = empList2.iterator();
+        while (empIter.hasNext()) {
             System.out.println(empIter.next());
-            if (!empList.contains("Tom Smith"))
-               empList.add("Tom Smith");
-         }
-         catch (ConcurrentModificationException cme)
-         {
-            System.err.println("attempt to modify list during iteration");
-            break;
-         }
+            if (!empList2.contains("Tom Smith"))
+                empList2.add("Tom Smith");
+        }
 
-      List<String> empList2 = new CopyOnWriteArrayList<>();
-      empList2.add("John Doe");
-      empList2.add("Jane Doe");
-      empList2.add("Rita Smith");
-      empIter = empList2.iterator();
-      while (empIter.hasNext())
-      {
-         System.out.println(empIter.next());
-         if (!empList2.contains("Tom Smith"))
-            empList2.add("Tom Smith");
-      }
-
-   }
+    }
 }
 
 /*
